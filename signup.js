3a5bf5e5e0c9f3c57f5ebf01ced1635e5a4d21c3
ismail <ismailelboutayeby@gmail.com>
2026-06-05 20:28:19 +0100

    Refactor signup functionality and update form ID in HTML

--- signup.js
+++ signup.js
@@ -1,66 +1,49 @@
 
+// let users = []
+// let Name = document.querySelector("#login").elements.name
+// let Email = document.querySelector("#login").elements.email
+// let Password = document.querySelector("#login").elements.password
+
+// let isinlockalestorage = localStorage.getItem('Users')
+
+
+
+
+
+
+
 let users = []
-let Name = document.querySelector("#login").elements.name
-let Email = document.querySelector("#login").elements.email
-let Password = document.querySelector("#login").elements.password
-
-let isinlockalestorage = localStorage.getItem('Users')
+let Name = document.querySelector("#signup").elements.name
+let Email = document.querySelector("#signup").elements.email
+let Password = document.querySelector("#signup").elements.password
 
 
 
-let signup = (event) => {
-
-
+let signup = (event)=>{
 
     event.preventDefault()
 
+  users.push({
+    Name: Name.value ,
+    Email: Email.value ,
+    Password: Password.value 
+  })
 
 
-    console.log("usersLocalStorage : ", isinlockalestorage);
 
-
-    if (isinlockalestorage === null) {
-        localStorage.setItem("Users", JSON.stringify(
-            [{
-                'Name': Name.value,
-                'Email': Email.value,
-                'Password': Password.value,
-            }]
-        ))
-
-        console.log('the first user are created');
-
-    } else {
-
-        users = [JSON.parse(localStorage.getItem('users'))]
-
-        users.push(
-            {
-                Name: Name.value,
-                Email: Email.value,
-                Password: Password.value,
-            }
-        )
-
-
-        console.log(users);
-        
-
-        localStorage.setItem("Users", JSON.stringify(
-            users
-        ))
-
-
-        console.log('another user created');
-
-    }
-
+  localStorage.setItem( "Users",JSON.stringify(users))
+    users =JSON.parse( localStorage.getItem( "Users"))
+    // console.log(users); 
+    
+  console.log(localStorage.length);
+  
+  console.log(users);
+  
 }
 
 
 
 
 
-
-
+    console.log(users);
 
